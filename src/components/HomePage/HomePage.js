b6ec7d09cecf4a440c4d1a96d084ff2a9af813bb
unknown <anndymo@gmail.com>
2019-12-10 10:05:17 -0800

    fixing home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -16,18 +16,22 @@ const HomePage = () => {
             <div className="pole">
             <img src={source} alt="" />
             </div>
-            <div className="pole1">
+            {/* <div className="pole1">
             <img src={source} alt="" />
-            </div>
+            </div> */}
             <div className="board" style={{ backgroundImage: `url(${board})` }}>
                 <div className="pricingMenu">
                     <h2 className="title"> <b>RT Cuts</b> </h2>
                     <p className="menu-items"> Price: $35 </p>
+                    <p className="menu-description">Here at RT Cuts, I provide only the best and greatest haircuts in San Francisco. Not only 
+                    are my cuts good, they're also fast! In 30 quick minutes you will be feeling yourself like you're Mac Dre. Check out a few quick snaps
+                    of my work below.
+                    </p>
                 </div>
                 <div className="container-holder">
                 <div className="row img-container">
                 <img src={haircut4} alt="" className="picture" />
-                <img src={haircut3} alt="" className="picture" />
+                <img src={haircut3} alt="" className="picture2" />
                 <img src={haircut5} alt="" className="picture" />
                 </div>
                 </div>
